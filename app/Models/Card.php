bf667e1fbfba85f6c5e93fdc5e8258129845c6e4
Anastasya <aamokeeva@yandex.ru>
2024-02-23 15:57:36 +0300

    Создает модели и миграции

---
+++ app/Models/Card.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'group_id',
+        'title',
+        'sort'
+    ];
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+}
