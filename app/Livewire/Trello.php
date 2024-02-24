3fabfeef8110288ddadf5abfa9668ac5c793959f
Anastasya <aamokeeva@yandex.ru>
2024-02-24 13:01:04 +0300

    Добавляет сортировку групп и карточек

--- app/Livewire/Trello.php
+++ app/Livewire/Trello.php
@@ -33,6 +33,29 @@ class Trello extends Component
         Card::destroy($cardId);
     }
 
+    /**
+     * Сортирует группы и карточки
+     * @param array $order приходит из плагина livewire-sortable
+     * при изменении порядка групп или карточек
+     * содержит id элемента (ключ 'value')
+     * и обновленный порядок элементов (ключ 'order')
+     * @return void
+     */
+    public function sorting(array $order): void
+    {
+        foreach ($order as $group) {
+            Group::where(['id' => $group['value']])
+                ->update(['sort' => $group['order']]);
+
+            if (isset($group['items'])) {
+                foreach ($group['items'] as $card) {
+                    Card::where(['id' => $card['value']])
+                        ->update(['sort' => $card['order']]);
+                }
+            }
+        }
+    }
+
     public function render()
     {
         $groups = Group::orderBy('sort')->get();
--- resources/views/livewire/trello.blade.php
+++ resources/views/livewire/trello.blade.php
@@ -7,21 +7,38 @@
         Add Desk
     </a>
 
-    <div class="flex py-8 items-start">
+    <div
+        class="flex py-8 items-start"
+        wire:sortable="sorting" wire:sortable-group="sorting"
+    >
         @foreach($groups as $group)
-            <div class="rounded bg-slate-50 flex-no-shrink w-64 p-2 mr-3">
+            <div
+                class="rounded bg-slate-50 flex-no-shrink w-64 p-2 mr-3"
+                wire:key="group-{{ $group->id }}" wire:sortable.item="{{ $group->id }}"
+            >
                 <div class="flex justify-between py-1">
-                    <h3 class="font-bold text-green-700">{{ $group->title }}</h3>
+                    <h3 class="font-bold text-green-700 cursor-pointer"
+                        wire:sortable.handle
+                    >
+                        {{ $group->title }}
+                    </h3>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"
                      wire:click="deleteGroup({{ $group->id }})"
                     >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>
                 </div>
-                <div class="text-sm mt-2">
+
+                <div class="text-sm mt-2"
+                     wire:sortable-group.item-group="{{ $group->id }}"
+                >
                     @foreach($group->cards as $card)
-                        <div class="flex bg-white p-2 justify-between rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
-                            <span>{{ $card->title }}</span>
+                        <div class="flex bg-white p-2 justify-between rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter"
+                             wire:key="card-{{ $card->id }}" wire:sortable-group.item="{{ $card->id }}"
+                        >
+                            <h4 class="flex cursor-pointer">
+                                {{ $card->title }}
+                            </h4>
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"
                                  wire:click="deleteCard({{ $card->id }})"
@@ -32,7 +49,9 @@
                     @endforeach
 
                     <button
-                        class="mt-3 text-grey-dark">
+                        class="mt-3 text-grey-dark"
+                        wire:click="addCard({{ $group->id }})"
+                    >
                         Add a card...
                     </button>
                 </div>
